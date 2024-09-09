e422b6d607a82a247ee235482b4a3050409cecd6
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-09-09 12:02:29 +0200

    choices scrapper adding error list

--- src/views/ChoicesScrapper.jsx
+++ src/views/ChoicesScrapper.jsx
@@ -3,7 +3,7 @@ import CHOICES_ICON from "../assets/choices.svg";
 import { Button, Divider, Input, Spinner } from "@nextui-org/react";
 import { UploadFileDiv } from "../components/UploadFileDiv";
 import * as XLSX from "xlsx/xlsx.mjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useClearNotation } from "../hooks/useClearNotation";
 import { motion, AnimatePresence } from "framer-motion";
 import { SuccessMessage } from "../components/SuccessMessage";
@@ -11,12 +11,18 @@ import { ErrorMessage } from "../components/ErrorMessage";
 
 export const ChoicesScrapper = () => {
   const [idArray, setIdArray] = useState([]);
-  const [login, setlogin] = useState(null);
+  const [login, setlogin] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [result, setResult] = useState([]);
   const { isError, setIsError, isSuccess, setIsSuccess } = useClearNotation();
+  const [isSendButtonAvailable, setIsSendButtonAvailable] = useState(true);
+  const [errorIdArray, setErrorIdArray] = useState([]);
+
+  useEffect(() => {
+    setIsSendButtonAvailable(true);
+  }, [idArray]);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -35,31 +41,39 @@ export const ChoicesScrapper = () => {
   };
 
   const downloadXLSX = () => {
+    const errorsValue = Object.values(...errorIdArray);
+    const errorsKey = Object.keys(...errorIdArray);
+
+    let finalErrorArray = [["ID", "Error_message"]];
+    for (let i = 0; i < errorsKey.length; i++) {
+      const id = errorsKey[i];
+      const rows = errorsValue[i];
+      rows.forEach((row) => {
+        finalErrorArray.push([id, row[0], row[1]]);
+      });
+    }
+
     const arrayOfArrays = Object.values(result);
     const keys = Object.keys(result);
-
     let finalArray = [["ID", "Variable_slug", "Choice_name", "Choice_slug"]];
-
     for (let i = 0; i < keys.length; i++) {
       const id = keys[i];
       const rows = arrayOfArrays[i];
-
       rows.forEach((row) => {
         finalArray.push([id, row[0], row[1], row[2]]);
       });
     }
-    const ws = XLSX.utils.aoa_to_sheet(finalArray);
 
+    const ws1 = XLSX.utils.aoa_to_sheet(finalArray);
+    const ws2 = XLSX.utils.aoa_to_sheet(finalErrorArray);
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-
+    XLSX.utils.book_append_sheet(wb, ws1, "Result");
+    XLSX.utils.book_append_sheet(wb, ws2, "Errors");
     const xlsx = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-
     const blob = new Blob([xlsx], {
       type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8;",
     });
     const url = URL.createObjectURL(blob);
-
     const a = document.createElement("a");
     a.href = url;
     a.download = "data_file.xlsx";
@@ -77,7 +91,7 @@ export const ChoicesScrapper = () => {
     try {
       setIsLoading(true);
       const req = await fetch(
-        "https://karolkrusz-choices-scrapper.hf.space/test/",
+        "https://karolkrusz-choices-scrapper.hf.space/choices-scrapper/",
         {
           method: "POST",
           headers: {
@@ -91,10 +105,17 @@ export const ChoicesScrapper = () => {
         }
       );
       if (req.ok) {
-        req.json().then((data) => setResult(data));
+        req.json().then((res) => {
+          console.log(res);
+          setResult(res.data);
+          setErrorIdArray(res.errors);
+        });
         setIsSuccess(true);
+        setIsSendButtonAvailable(false);
       } else if (req.status === 420) {
         throw new Error("Login error, check credentials");
+      } else {
+        throw new Error("Error, please contact with developer");
       }
     } catch (error) {
       setIsError(true);
@@ -125,8 +146,15 @@ export const ChoicesScrapper = () => {
         />
         <UploadFileDiv width="w-full" onChange={handleFileChange} />
 
-        <Button onClick={sendData}>Send</Button>
-        <Button onClick={downloadXLSX}>Download</Button>
+        <Button
+          onClick={sendData}
+          isDisabled={!isSendButtonAvailable || isLoading}
+        >
+          Send
+        </Button>
+        <Button onClick={downloadXLSX} isDisabled={isSendButtonAvailable}>
+          Download
+        </Button>
 
         {isLoading && <Spinner label="Loading" color="default" size="lg" />}
         <AnimatePresence>
